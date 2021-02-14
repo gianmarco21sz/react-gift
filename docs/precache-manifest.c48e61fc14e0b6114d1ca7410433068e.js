a09477e67479a2d7df275942b3b12943ee528a6b
Gian Marco <gianmarco21sz@gmail.com>
2021-02-13 22:31:47 -0500

    docs agregado

---
+++ docs/precache-manifest.c48e61fc14e0b6114d1ca7410433068e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c2a7fb94a2f9dc579aec640f6a7ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b87e6045bde4a46eab9a",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b87e6045bde4a46eab9a",
+    "url": "/static/js/main.3697053f.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
